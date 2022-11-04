877b08ce1cdbf629f60c19af367a3035d1a74f6f
discodyer <cody23333@gmail.com>
2022-11-04 17:37:43 +0800

    修复markdown错位

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -71,12 +71,12 @@ export default hopeTheme({
     // If you don’t need comment feature, you can remove following option
     // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
     // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
-    comment: {
-      provider: "Waline",
-      serverURL: "https://api.doc.sastit.com",
-      pageview: true,
-      comment: false,
-    },
+    // comment: {
+    //   provider: "Waline",
+    //   serverURL: "https://api.doc.sastit.com",
+    //   pageview: true,
+    //   comment: false,
+    // },
 
     // Disable features you don’t want here
     mdEnhance: {
